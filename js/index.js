99348f2ca2421fd15413cc35e8574b35301eaafa
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-15 22:29:09 +0900

    Refactor directory data structure and update related UI components; enhance avatar paths and improve loading functionality in index.js and about.html

--- js/index.js
+++ js/index.js
@@ -2,47 +2,34 @@ const currentYear = document.getElementById("current__year");
 
 currentYear.textContent = new Date().getFullYear();
 
-const cards__container = document.getElementById("cards__container");
-// const template = document.getElementById("cards__template");
 const template = document.querySelector("template");
+const cards__container = document.getElementById("cards__container");
 
-function updateUi(directory) {
-  directory.forEach((director) => {
+function updateUI(directory) {
+  directory.forEach((item) => {
     const clone = template.content.cloneNode(true);
 
     const directory__img = clone.querySelector(".directory__img");
     const directory__name = clone.querySelector(".directory__name");
     const directory__position = clone.querySelector(".directory__position");
 
-    directory__img.src = director.avatar;
-    directory__name.textContent = director.name;
-    directory__position.textContent = director.position;
+    directory__img.src = item.avatar;
+    directory__name.textContent = item.name;
+    directory__position.textContent = item.position;
 
     cards__container.appendChild(clone);
   });
 }
 
-function loading(status = false) {
-  if (status) {
-    document.querySelector(".loader").classList.remove("hidden");
-  } else {
-    document.querySelector(".loader").classList.add("hidden");
-  }
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-  loading(true);
   fetch("http://localhost:3000/directory")
     .then((data) => {
       return data.json();
     })
     .then((directory) => {
-      updateUi(directory);
+      updateUI(directory);
     })
     .catch((error) => {
       console.log(error);
-    })
-    .finally(() => {
-      loading(false);
     });
 });
